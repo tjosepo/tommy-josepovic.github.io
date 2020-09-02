278eef0c35549863d8d72fd3a8fd426a8bfb83d9
tommy-josepovic <tommy-josepovic@users.noreply.github.com>
2020-09-02 10:51:36 -0400

    Add Shopify Challenge stuff

---
+++ shopify-challenge/precache-manifest.cdcb7004f53c35a6d4063c9fa3acecb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5caecf8a6db3b05fa0a37a866a94be90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0cf84a9e4ecb061cbd7",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "27511658015f86992176",
+    "url": "/static/css/main.cc09a07c.chunk.css"
+  },
+  {
+    "revision": "d0cf84a9e4ecb061cbd7",
+    "url": "/static/js/2.ae07b878.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/static/js/2.ae07b878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27511658015f86992176",
+    "url": "/static/js/main.aba47d1f.chunk.js"
+  },
+  {
+    "revision": "f5ffb3e6f63b8e8032df",
+    "url": "/static/js/runtime-main.36f2b9f2.js"
+  }
+]);
